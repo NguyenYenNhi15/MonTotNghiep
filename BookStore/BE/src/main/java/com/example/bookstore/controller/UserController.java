3d4b8d614a10d8a59ec5d132ea74e3d7b895fedf
Nguyễn Thị Yến Nhi <70928788+NguyenYenNhi15@users.noreply.github.com>
2023-11-25 21:00:08 +0700

    commit

--- BookStore/BE/src/main/java/com/example/bookstore/controller/UserController.java
+++ BookStore/BE/src/main/java/com/example/bookstore/controller/UserController.java
@@ -6,10 +6,12 @@ import com.example.bookstore.request.SignUpRequest;
 import com.example.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class UserController {
     @Autowired
